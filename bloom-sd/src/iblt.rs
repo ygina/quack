5431b4763910df015794f3c02689a8b228ed7a51
Gina Yuan <gina.yuan@gmail.com>
2022-04-06 10:55:20 -0700

    biguint in iblt data field

--- bloom-sd/src/iblt.rs
+++ bloom-sd/src/iblt.rs
@@ -2,16 +2,16 @@ use std::hash::BuildHasher;
 use std::collections::HashSet;
 use std::collections::hash_map::RandomState;
 use bloom::valuevec::ValueVec;
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::{One, Zero};
 use crate::hashing::HashIter;
-
-/// The image of the packet hash.
-type Packet = u32;
 
 /// TODO: Fatally, assumes inserted elements are unique.
 /// Elements are u32s.
 pub struct InvBloomLookupTable<R = RandomState, S = RandomState> {
     counters: ValueVec,
-    data: Vec<Packet>,
+    max_data_value: BigUint,
+    data: Vec<BigUint>,
     num_entries: u64,
     num_hashes: u32,
     hash_builder_one: R,
@@ -24,6 +24,7 @@ impl InvBloomLookupTable<RandomState, RandomState> {
     /// will be sized to have a false positive rate of the value specified
     /// in `rate`.
     pub fn with_rate(
+        log2_data_length: u32,
         bits_per_entry: usize,
         rate: f32,
         expected_num_items: u32,
@@ -35,7 +36,9 @@ impl InvBloomLookupTable<RandomState, RandomState> {
             expected_num_items,
         );
         InvBloomLookupTable {
-            data: vec![0; num_entries],
+            max_data_value: 2_u8.to_biguint().unwrap().pow(log2_data_length)
+                - BigUint::one(),
+            data: vec![BigUint::zero(); num_entries],
             counters: ValueVec::new(bits_per_entry, num_entries),
             num_entries: num_entries as u64,
             num_hashes,
@@ -48,7 +51,8 @@ impl InvBloomLookupTable<RandomState, RandomState> {
     pub fn empty_clone(&self) -> Self {
         let bits_per_entry = self.counters.bits_per_val();
         Self {
-            data: vec![0; self.num_entries as usize],
+            max_data_value: self.max_data_value.clone(),
+            data: vec![0.to_biguint().unwrap(); self.num_entries as usize],
             counters: ValueVec::new(bits_per_entry, self.num_entries as usize),
             num_entries: self.num_entries,
             num_hashes: self.num_hashes,
@@ -57,11 +61,11 @@ impl InvBloomLookupTable<RandomState, RandomState> {
         }
     }
 
-    pub fn data(&self) -> &Vec<u32> {
+    pub fn data(&self) -> &Vec<BigUint> {
         &self.data
     }
 
-    pub fn data_mut(&mut self) -> &mut Vec<u32> {
+    pub fn data_mut(&mut self) -> &mut Vec<BigUint> {
         &mut self.data
     }
 
@@ -85,7 +89,7 @@ impl InvBloomLookupTable<RandomState, RandomState> {
 impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
     /// Inserts an item, returns true if the item was already in the filter
     /// any number of times.
-    pub fn insert(&mut self, item: &Packet) -> bool {
+    pub fn insert(&mut self, item: &BigUint) -> bool {
         let mut min = u32::max_value();
         for h in HashIter::from(item,
                                 self.num_hashes,
@@ -98,7 +102,7 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
             }
             if cur < self.counters.max_value() {
                 self.counters.set(idx, cur + 1);
-                let difference = u32::MAX - self.data[idx];
+                let difference = &self.max_data_value - &self.data[idx];
                 if difference > *item {
                     self.data[idx] += item;
                 } else {
@@ -112,7 +116,7 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
     }
 
     /// Removes an item, panics if the item does not exist.
-    pub fn remove(&mut self, item: &Packet) {
+    pub fn remove(&mut self, item: &BigUint) {
         for h in HashIter::from(item,
                                 self.num_hashes,
                                 &self.hash_builder_one,
@@ -126,14 +130,14 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
             if self.data[idx] >= *item {
                 self.data[idx] -= item;
             } else {
-                self.data[idx] = u32::MAX - (item - self.data[idx]);
+                self.data[idx] = &self.max_data_value - (item - &self.data[idx]);
             }
         }
     }
 
     /// Checks if the item has been inserted into this InvBloomLookupTable.
     /// This function can return false positives, but not false negatives.
-    pub fn contains(&self, item: &Packet) -> bool {
+    pub fn contains(&self, item: &BigUint) -> bool {
         for h in HashIter::from(item,
                                 self.num_hashes,
                                 &self.hash_builder_one,
@@ -148,7 +152,7 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
     }
 
     /// Gets the indexes of the item in the vector.
-    pub fn indexes(&self, item: &Packet) -> Vec<usize> {
+    pub fn indexes(&self, item: &BigUint) -> Vec<usize> {
         HashIter::from(item,
                        self.num_hashes,
                        &self.hash_builder_one,
@@ -161,17 +165,17 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
     /// Enumerates as many items as possible in the IBLT and removes them.
     /// Returns the removed items. Note removed elements must be unique
     /// unless the IBLT uses an accumulator function that is not an XOR.
-    pub fn eliminate_elems(&mut self) -> HashSet<Packet> {
+    pub fn eliminate_elems(&mut self) -> HashSet<BigUint> {
         // Loop through all the counters of the IBLT until there are no
         // remaining cells with count 1. This is O(num_counters*max_count).
-        let mut removed_set: HashSet<Packet> = HashSet::new();
+        let mut removed_set: HashSet<BigUint> = HashSet::new();
         loop {
             let mut removed = false;
             for i in 0..(self.num_entries as usize) {
                 if self.counters.get(i) != 1 {
                     continue;
                 }
-                let item = self.data[i];
+                let item = self.data[i].clone();
                 self.remove(&item);
                 assert!(removed_set.insert(item));
                 removed = true;
@@ -186,9 +190,10 @@ impl<R,S> InvBloomLookupTable<R,S> where R: BuildHasher, S: BuildHasher {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num_traits::Zero;
 
     fn init_iblt() -> InvBloomLookupTable {
-        InvBloomLookupTable::with_rate(8, 0.01, 10)
+        InvBloomLookupTable::with_rate(128, 8, 0.01, 10)
     }
 
     fn vvsum(vec: &ValueVec) -> usize {
@@ -202,51 +207,53 @@ mod tests {
         assert_eq!(iblt.num_entries(), 96);
         assert_eq!(iblt.num_hashes(), 2);
         assert_eq!(vvsum(iblt.counters()), 0);
-        assert_eq!(iblt.data().iter().sum::<u32>(), 0);
+        assert_eq!(iblt.data().iter().sum::<BigUint>(), BigUint::zero());
         assert_eq!(iblt.data().len(), iblt.num_entries() as usize);
     }
 
     #[test]
     fn test_insert() {
         let mut iblt = init_iblt();
-        let elem = 1234;
+        let elem = 1234_u32.to_biguint().unwrap();
         let indexes = iblt.indexes(&elem);
         for &idx in &indexes {
             assert_eq!(iblt.counters().get(idx), 0);
-            assert_eq!(iblt.data()[idx], 0);
+            assert_eq!(iblt.data()[idx], BigUint::zero());
         }
         assert!(!iblt.insert(&elem), "element did not exist already");
         assert_eq!(vvsum(iblt.counters()), 1 * iblt.num_hashes() as usize);
         for &idx in &indexes {
             assert_ne!(iblt.counters().get(idx), 0);
-            assert_ne!(iblt.data()[idx], 0);
+            assert_ne!(iblt.data()[idx], BigUint::zero());
         }
         assert!(iblt.insert(&elem), "added element twice");
         assert_eq!(vvsum(iblt.counters()), 2 * iblt.num_hashes() as usize);
         for &idx in &indexes {
             assert_ne!(iblt.counters().get(idx), 0);
-            assert_ne!(iblt.data()[idx], 0);
+            assert_ne!(iblt.data()[idx], BigUint::zero());
         }
     }
 
     #[test]
     fn test_empty_clone() {
         let mut iblt1 = init_iblt();
-        iblt1.insert(&1234);
-        iblt1.insert(&5678);
+        iblt1.insert(&1234_u32.to_biguint().unwrap());
+        iblt1.insert(&5678_u32.to_biguint().unwrap());
         let iblt2 = iblt1.empty_clone();
         assert!(vvsum(iblt1.counters()) > 0);
         assert_eq!(vvsum(iblt2.counters()), 0);
-        assert!(iblt1.data().iter().sum::<u32>() > 0);
-        assert_eq!(iblt2.data().iter().sum::<u32>(), 0);
-        assert_eq!(iblt1.indexes(&1234), iblt2.indexes(&1234));
+        assert!(iblt1.data().iter().sum::<BigUint>() > BigUint::zero());
+        assert_eq!(iblt2.data().iter().sum::<BigUint>(), BigUint::zero());
+        assert_eq!(
+            iblt1.indexes(&1234_u32.to_biguint().unwrap()),
+            iblt2.indexes(&1234_u32.to_biguint().unwrap()));
     }
 
     #[test]
     #[should_panic]
     fn counter_overflow() {
-        let mut iblt = InvBloomLookupTable::with_rate(1, 0.01, 10);
-        iblt.insert(&1234);
-        iblt.insert(&1234);
+        let mut iblt = InvBloomLookupTable::with_rate(128, 1, 0.01, 10);
+        iblt.insert(&1234_u32.to_biguint().unwrap());
+        iblt.insert(&1234_u32.to_biguint().unwrap());
     }
 }
