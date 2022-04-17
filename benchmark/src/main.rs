914f75dd2e84587a0f186233ab65719f9f6d6204
Gina Yuan <gina.yuan@gmail.com>
2022-04-16 18:03:08 -0700

    benchmark validation over multiple trials

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -3,10 +3,67 @@ extern crate log;
 
 pub mod generator;
 
-use std::time::Instant;
+use std::time::{Instant, Duration};
 use clap::{Arg, Command};
 use accumulator::*;
 use generator::LoadGenerator;
+
+fn build_accumulator(
+    g: &mut LoadGenerator,
+    accumulator_ty: &str,
+    threshold: usize,
+) -> Box<dyn Accumulator> {
+    let mut accumulator: Box<dyn Accumulator> = {
+        match accumulator_ty {
+            "naive" => Box::new(NaiveAccumulator::new()),
+            "cbf" => Box::new(CBFAccumulator::new(threshold)),
+            "iblt" => Box::new(IBLTAccumulator::new(threshold)),
+            "power_sum" => Box::new(PowerSumAccumulator::new(threshold)),
+            _ => unreachable!(),
+        }
+    };
+    let t1 = Instant::now();
+    while let Some(elem) = g.next() {
+        accumulator.process(&elem);
+    }
+    let t2 = Instant::now();
+    debug!(
+        "dropped {}/{} elements: {:?}",
+        g.num_dropped,
+        g.num_logged,
+        t2 - t1,
+    );
+    info!("digest size = {} bytes", accumulator.to_bytes().len());
+    accumulator
+}
+
+/// Validate the log against the accumulator.
+fn validate(
+    accumulator: Box<dyn Accumulator>,
+    elems: &Vec<Vec<u8>>,
+    malicious: bool,
+) -> Result<Duration, ()> {
+    let t1 = Instant::now();
+    let valid = accumulator.validate(elems);
+    let total = Instant::now() - t1;
+    if valid == !malicious {
+        info!("validation is correct ({}): {:?}", valid, total);
+        Ok(total)
+    } else {
+        error!("validation failed, expected {}", !malicious);
+        Err(())
+    }
+}
+
+fn median(mut results: Vec<Duration>) -> Duration {
+    results.sort();
+    let mid = results.len() / 2;
+    if results.len() & 1 == 0 {
+        (results[mid] + results[mid + 1]) / 2
+    } else {
+        results[mid]
+    }
+}
 
 fn main() {
     env_logger::builder().filter_level(log::LevelFilter::Debug).init();
@@ -36,6 +93,11 @@ fn main() {
             .long("threshold")
             .takes_value(true)
             .default_value("1000"))
+        .arg(Arg::new("trials")
+            .help("Number of trials to run. Reports the median.")
+            .long("trials")
+            .takes_value(true)
+            .default_value("1"))
         .arg(Arg::new("accumulator")
             .help("")
             .short('a')
@@ -48,6 +110,7 @@ fn main() {
             .required(true))
         .get_matches();
 
+    let trials: usize = matches.value_of("trials").unwrap().parse().unwrap();
     let num_logged: usize = matches.value_of("num-logged").unwrap()
         .parse().unwrap();
     let p_dropped: f32 = matches.value_of("p-dropped").unwrap()
@@ -57,35 +120,16 @@ fn main() {
     debug!("p_dropped = {}", p_dropped);
     debug!("malicious = {}", malicious);
 
-    let mut accumulator: Box<dyn Accumulator> = {
-        let threshold: usize = matches.value_of("threshold").unwrap()
-            .parse().unwrap();
-        match matches.value_of("accumulator").unwrap() {
-            "naive" => Box::new(NaiveAccumulator::new()),
-            "cbf" => Box::new(CBFAccumulator::new(threshold)),
-            "iblt" => Box::new(IBLTAccumulator::new(threshold)),
-            "power_sum" => Box::new(PowerSumAccumulator::new(threshold)),
-            _ => unreachable!(),
-        }
-    };
-    let mut g = LoadGenerator::new(num_logged, p_dropped, malicious);
-    let t1 = Instant::now();
-    while let Some(elem) = g.next() {
-        accumulator.process(&elem);
-    }
-    let t2 = Instant::now();
-    debug!("dropped {}/{} elements", g.num_dropped, g.num_logged);
-    info!("processed {} elements in {:?}",
-        g.num_logged - g.num_dropped, t2 - t1);
+    let accumulator_ty = matches.value_of("accumulator").unwrap();
+    let threshold: usize = matches.value_of("threshold").unwrap()
+        .parse().unwrap();
 
-    // Validate the log against the accumulator.
-    let valid = accumulator.validate(&g.log);
-    let t3 = Instant::now();
-    info!("validation took {:?}", t3 - t2);
-    if valid == !malicious {
-        info!("validation is correct ({})", valid);
-    } else {
-        error!("validation failed, expected {}", !malicious);
-    }
-    info!("digest size = {} bytes", accumulator.to_bytes().len());
+    let results: Vec<_> = (0..trials)
+        .map(|_| {
+            let mut g = LoadGenerator::new(num_logged, p_dropped, malicious);
+            let acc = build_accumulator(&mut g, accumulator_ty, threshold);
+            validate(acc, &g.log, malicious).unwrap()
+        })
+        .collect();
+    info!("median\t{}\t{}\t{:?}", num_logged, p_dropped, median(results));
 }
